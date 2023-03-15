0d76417c5d96f7104cf5bf65e039993400f6ee0b
ryks4lyfe <rykerynwa@gmail.com>
2023-03-15 14:17:32 -0400

    schedule_builder

---
+++ schedule_builder/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from schedule_builder import views
+
+app_name = 'schedule_builder'
+urlpatterns = [
+    path('', views.index, name='schedule_index'), 
+    path('all_events/', views.all_events, name='all_events'), 
+    path('add_event/', views.add_event, name='add_event'), 
+    path('update/', views.update, name='update'),
+    path('remove/', views.remove, name='remove'),
+]
