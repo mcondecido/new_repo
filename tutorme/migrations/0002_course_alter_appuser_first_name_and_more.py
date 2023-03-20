56c3a25396795d1a86dc1d66ecfff17b0f077a1f
Carole Yin <caroleyin010@gmail.com>
2023-03-20 12:49:32 -0400

    fix migrations

--- tutorme/migrations/0002_course_alter_appuser_first_name_and_more.py
+++ tutorme/migrations/0002_course_alter_appuser_first_name_and_more.py
@@ -10,20 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Course',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('subject', models.CharField(max_length=10)),
-                ('catalog_number', models.CharField(max_length=10)),
-                ('course_id', models.CharField(max_length=10)),
-                ('is_enrolled', models.BooleanField(default=False)),
-                ('meeting_days', models.CharField(max_length=10)),
-                ('start_time', models.CharField(max_length=200)),
-                ('end_time', models.CharField(max_length=200)),
-            ],
-        ),
         migrations.AlterField(
             model_name='appuser',
             name='first_name',
